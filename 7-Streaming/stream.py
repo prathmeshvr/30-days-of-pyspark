1ea224ae7343d3d4e7a8b58181b626fc626fb694
Prathmesh Rahate <pvr684@gmail.com>
2021-04-26 00:16:13 +0530

    day 14

---
+++ 7-Streaming/stream.py
@@ -0,0 +1,15 @@
+import os
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+import json
+sc=SparkContext(master='local[*]',appName='test')
+ssc=StreamingContext(sc,batchDuration=20)
+brokers='192.168.0.103:9092'
+topic="topic_test"
+kvs=KafkaUtils.createDirectStream(ssc,[topic],kafkaParams={"metadata.broker.list":brokers})
+kvs.pprint()
+ssc.start()
+ssc.awaitTermination()
+
+# spark-submit --jars /home/pratham/PersonalSpace/30-days-of-pyspark/7-Streaming/spark-streaming-kafka-0-8-assembly_2.11-2.4.7.jar /home/pratham/PersonalSpace/30-days-of-pyspark/7-Streaming/stream.py
